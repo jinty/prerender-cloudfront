a3cfcc2cc054c1e6be4d35e00fc47aec3a481064
Brian Sutherland <brian@vanguardistas.net>
2018-04-04 11:17:16 +0200

    simplify our SPA html code
    This makes it more obvious that the example is pre-rendered
    
    refs https://github.com/jinty/prerender-cloudfront/issues/4

--- code.js
+++ code.js
@@ -4,13 +4,13 @@
     if (paths.indexOf(current) === -1) {
         content = '<p>404 Not Found</p>';
     } else {
-            content = '<p>Welcome to the ' + current + ' page.<p>';
+            content = '<p>Welcome to the ' + current + ' page.<p>\n';
     }
-    content = content + '<ul>';
+    content = content + '<ul>\n';
     for (let s of paths) {
-        content = content + '<li><a href="' + s + '">' + s + '</a></li>';
+        content = content + '<li><a href="' + s + '">' + s + '</a></li>\n';
     }
     content = content + '</ul>';
     document.getElementById('content').innerHTML = content;
-
+    document.getElementById('header').innerHTML = 'Simplest SPA ever';
 })();
